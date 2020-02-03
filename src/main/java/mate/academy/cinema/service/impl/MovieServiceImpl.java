dc51f7d8e9038a13f6ef706d3e9654bf184aeed9
ElvenNurse <58395217+ElvenNurse@users.noreply.github.com>
2020-02-03 17:56:41 +0200

    Configured Hibernate Created Movie model and DAO/Service layers

---
+++ src/main/java/mate/academy/cinema/service/impl/MovieServiceImpl.java
@@ -0,0 +1,23 @@
+package mate.academy.cinema.service.impl;
+
+import java.util.List;
+
+import mate.academy.cinema.dao.MovieDao;
+import mate.academy.cinema.lib.Inject;
+import mate.academy.cinema.model.Movie;
+import mate.academy.cinema.service.MovieService;
+
+public class MovieServiceImpl implements MovieService {
+    @Inject
+    private static MovieDao movieDao;
+
+    @Override
+    public Movie add(Movie movie) {
+        return movieDao.add(movie);
+    }
+
+    @Override
+    public List<Movie> getAll() {
+        return movieDao.getAll();
+    }
+}
