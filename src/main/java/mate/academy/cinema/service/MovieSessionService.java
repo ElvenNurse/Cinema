3b211ab2ecdd4dd6ca1e5d9ce22df1bc7514fb0e
ElvenNurse <58395217+ElvenNurse@users.noreply.github.com>
2020-02-04 17:13:27 +0200

    Added CinemaHall and MovieSession models Implemented DAO and Service layers for CinemaHall and MovieSession

---
+++ src/main/java/mate/academy/cinema/service/MovieSessionService.java
@@ -0,0 +1,12 @@
+package mate.academy.cinema.service;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import mate.academy.cinema.model.MovieSession;
+
+public interface MovieSessionService {
+    MovieSession add(MovieSession session);
+
+    List<MovieSession> findAvailableSessions(Long movieId, LocalDate date);
+}
