120a434b205085bad069b78a0253c81289989a7d
Andrii Voloshyn <58395217+ElvenNurse@users.noreply.github.com>
2020-02-04 11:35:59 +0200

    Configured Hibernate (#1)
    Created Movie model and DAO/Service layers

---
+++ src/main/java/mate/academy/cinema/dao/impl/MovieDaoImpl.java
@@ -0,0 +1,45 @@
+package mate.academy.cinema.dao.impl;
+
+import java.util.List;
+
+import javax.persistence.criteria.CriteriaQuery;
+
+import mate.academy.cinema.dao.MovieDao;
+import mate.academy.cinema.exception.DataProcessingException;
+import mate.academy.cinema.lib.Dao;
+import mate.academy.cinema.model.Movie;
+import mate.academy.cinema.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+@Dao
+public class MovieDaoImpl implements MovieDao {
+    @Override
+    public Movie add(Movie movie) {
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            Long itemId = (Long) session.save(movie);
+            transaction.commit();
+            movie.setId(itemId);
+            return movie;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("Can't insert Movie entity", e);
+        }
+    }
+
+    @Override
+    public List<Movie> getAll() {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            CriteriaQuery<Movie> criteriaQuery = session.getCriteriaBuilder()
+                    .createQuery(Movie.class);
+            criteriaQuery.from(Movie.class);
+            return session.createQuery(criteriaQuery).getResultList();
+        } catch (Exception e) {
+            throw new DataProcessingException("Can't get all movies", e);
+        }
+    }
+}
